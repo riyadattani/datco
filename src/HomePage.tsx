ec9aeb03177876b4de2754f16e14fc7f86dc1412
Riya Dattani <riyaddattani@gmail.com>
2019-10-18 10:28:34 +0100

    add travis ci and remove unwanted files

---
+++ src/HomePage.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <div className="container">
+      <h1 className="page-header">Datco</h1>
+      <div className="flex-row">
+        <div className="flex-large">
+          <h2>About</h2>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default HomePage
